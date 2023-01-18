0d672f2082146277fd28b3aebd9bb8864c085682
Ma-tes <msestaubr@gmail.com>
2023-01-18 19:34:12 +0100

    Created a new uncomplete implementation of lexical token reader

--- src/Athena.NET/Athena.NET.Parser/LexicalAnalyzer/LexicalTokenReader.cs
+++ src/Athena.NET/Athena.NET.Parser/LexicalAnalyzer/LexicalTokenReader.cs
@@ -1,17 +1,42 @@
-﻿using System.Text;
+﻿using Athena.NET.Athena.NET.Parser.Structures;
+using System.Text;
 
 namespace Athena.NET.Athena.NET.Parser.LexicalAnalyzer
 {
-    internal abstract class LexicalTokenReader : StreamReader, IAsyncDisposable
+    internal abstract class LexicalTokenReader : IDisposable
     {
+        private static readonly string blankLine = "\0";
         private static readonly Encoding defaultEncoding =
             Encoding.UTF8;
 
-        public long ReaderPosition { get; }
+        private StreamReader streamReader;
 
-        public LexicalTokenReader(Stream stream) : base(stream, defaultEncoding, false)
+        public Memory<char> ReaderData { get; }
+
+        public long ReaderLength { get; }
+        public long ReaderPosition { get; private set; }
+
+        public LexicalTokenReader(Stream stream)
         {
-            ReaderPosition = stream.Position;
-        } 
+            streamReader = new(stream, defaultEncoding, false);
+
+            ReaderLength = streamReader.BaseStream.Length;
+            ReaderData = new char[ReaderLength];
+        }
+
+        public async Task<ReadOnlyMemory<Token>> ReadLexicalTokensAsync() 
+        {
+            await streamReader.ReadAsync(ReaderData);
+            while (ReaderPosition != ReaderLength) 
+            {
+            }
+        }
+
+        protected abstract Token GetToken(ReadOnlyMemory<char> data);
+
+        public void Dispose() 
+        {
+            streamReader.Dispose();
+        }
     }
 }
