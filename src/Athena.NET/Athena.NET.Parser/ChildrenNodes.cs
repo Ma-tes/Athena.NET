0cc965c8f5769200cbbcd6734a51e543e3a59550
Ma-tes <msestaubr@gmail.com>
2023-01-28 21:12:48 +0100

    Created uncomplete node folder structure

---
+++ src/Athena.NET/Athena.NET.Parser/ChildrenNodes.cs
@@ -0,0 +1,16 @@
+﻿using Athena.NET.Athena.NET.Parser.Interfaces;
+
+namespace Athena.NET.Athena.NET.Parser
+{
+    internal readonly struct ChildrenNodes
+    {
+        public INode LeftNode { get; }
+        public INode RightNode { get; }
+
+        public ChildrenNodes(INode leftNode, INode rightNode) 
+        {
+            LeftNode = leftNode;
+            RightNode = rightNode;
+        }
+    }
+}
---
+++ src/Athena.NET/Athena.NET.Parser/Interfaces/INode.cs
@@ -0,0 +1,10 @@
+﻿using Athena.NET.Athena.NET.Lexer.Structures;
+
+namespace Athena.NET.Athena.NET.Parser.Interfaces
+{
+    internal interface INode
+    {
+        public abstract Token NodeToken { get; }
+        public ChildrenNodes ChildNodes { get; }
+    }
+}
