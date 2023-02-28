61fe1fcb3b75eb239ca602e3d099ff91b82edc1c
Ma-tes <msestaubr@gmail.com>
2023-02-28 19:40:51 +0100

    Implemented all logical operators for parser

---
+++ src/Athena.NET/Athena.NET.Parser/Nodes/OperatorNodes/LogicalOperators/LessOperator.cs
@@ -0,0 +1,17 @@
+﻿using Athena.NET.Athena.NET.Lexer;
+
+namespace Athena.NET.Athena.NET.Parser.Nodes.OperatorNodes.LogicalOperators
+{
+    internal sealed class LessOperator : LogicalOperator
+    {
+        public override TokenIndentificator NodeToken { get; } =
+            TokenIndentificator.LessThan;
+
+        public LessOperator() 
+        {
+        }
+
+        protected override bool CalculateLogicalBool(int firstValue, int secondValue) =>
+            firstValue < secondValue;
+    }
+}
