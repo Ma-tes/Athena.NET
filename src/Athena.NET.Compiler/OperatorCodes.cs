0894984c309120f60ac12bcbbfd2a221b138b9bc
ma-tes <msestaubr@gmail.com>
2023-03-04 20:05:32 +0100

    Implemented uncomplete enum of operator codes

---
+++ src/Athena.NET.Compiler/OperatorCodes.cs
@@ -0,0 +1,18 @@
+﻿namespace Athena.NET.Athena.NET.Compiler
+{
+    internal enum OperatorCodes : uint
+    {
+        BPush = 0x01,
+        SPush = 0x02,
+        IPush = 0x03,
+        LPush = 0x04,
+
+        //I have a great idea of having
+        //two registers, so it will be just
+        //24 bits
+        R0Store = 0xA1, //Store data in register of 8 bits
+        R1Store = 0xA2, //Store data in register of 16 bits
+        R0Load = 0xB1,  //Load data from register of 8 bits
+        R1Load = 0xB2,  //Load data from register of 16 bits
+    }
+}
