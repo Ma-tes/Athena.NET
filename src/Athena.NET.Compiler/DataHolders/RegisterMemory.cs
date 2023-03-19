1d1d83bab8184e23de431587a5a0950da28eb0d9
Ma-tes <msestaubr@gmail.com>
2023-03-19 20:57:09 +0100

    Implemented uncomplete memory register for future interpreter

---
+++ src/Athena.NET.Compiler/DataHolders/RegisterMemory.cs
@@ -0,0 +1,46 @@
+﻿using Athena.NET.Athena.NET.Compiler.Structures;
+using Athena.NET.Compiler;
+using Athena.NET.Compiler.DataHolders;
+using System.Runtime.InteropServices;
+
+namespace Athena.NET.Athena.NET.Compiler.DataHolders
+{
+    internal sealed class RegisterMemory<T> : IDisposable 
+        where T : unmanaged
+    {
+        private readonly NativeMemoryList<ulong> registerMemoryList
+            = new();
+
+        public OperatorCodes RegisterCode { get; }
+        public int RegisterSize { get; }
+
+        public RegisterMemory(OperatorCodes registerCode) 
+        {
+            RegisterCode = registerCode;
+            RegisterSize = Marshal.SizeOf<T>() * 8;
+        }
+
+        public RegisterMemory(OperatorCodes registerCode, int allocationSize) 
+        {
+            RegisterCode = registerCode;
+            RegisterSize = allocationSize;
+        }
+
+        public void AddData(RegisterData registerData, int value) 
+        {
+            int totalMemorySize = registerData.Offset + registerData.Size;
+            int currentCount = registerMemoryList.Count;
+
+            if (currentCount == 0 ||
+                totalMemorySize > RegisterSize)
+                registerMemoryList.Add(default);
+
+            registerMemoryList.Span[currentCount - 1] += (ulong)(value >> registerData.Offset);
+        }
+
+        public void Dispose() 
+        {
+            registerMemoryList.Dispose();
+        }
+    }
+}
---
+++ src/Athena.NET.Compiler/Structures/RegisterData.cs
@@ -0,0 +1,14 @@
+﻿namespace Athena.NET.Athena.NET.Compiler.Structures
+{
+    internal readonly struct RegisterData
+    {
+        public int Offset { get; }
+        public int Size { get; }
+
+        public RegisterData(int offset, int size) 
+        {
+            Offset = offset;
+            Size = size;
+        }
+    }
+}
