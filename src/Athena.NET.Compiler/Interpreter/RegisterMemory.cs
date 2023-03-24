705b3f3def0abb61d796ec5486d581364b9ea3d5
Ma-tes <msestaubr@gmail.com>
2023-03-24 20:39:18 +0100

    Moved register memory into separete interpreter folder and deleted testing data in a main program

--- src/Athena.NET.Compiler/Interpreter/RegisterMemory.cs
+++ src/Athena.NET.Compiler/Interpreter/RegisterMemory.cs
@@ -3,9 +3,9 @@ using Athena.NET.Compiler;
 using Athena.NET.Compiler.DataHolders;
 using System.Runtime.InteropServices;
 
-namespace Athena.NET.Athena.NET.Compiler.DataHolders
+namespace Athena.NET.Athena.NET.Compiler.Interpreter
 {
-    internal sealed class RegisterMemory<T> : IDisposable 
+    internal sealed class RegisterMemory<T> : IDisposable
         where T : unmanaged
     {
         private readonly NativeMemoryList<ulong> registerMemoryList
@@ -14,7 +14,7 @@ namespace Athena.NET.Athena.NET.Compiler.DataHolders
         public OperatorCodes RegisterCode { get; }
         public int RegisterSize { get; }
 
-        public RegisterMemory(OperatorCodes registerCode) 
+        public RegisterMemory(OperatorCodes registerCode)
         {
             RegisterCode = registerCode;
             RegisterSize = Marshal.SizeOf<T>() * 8;
@@ -26,35 +26,35 @@ namespace Athena.NET.Athena.NET.Compiler.DataHolders
             RegisterSize = allocationSize;
         }
 
-        public void AddData(RegisterData registerData, int value) 
+        public void AddData(RegisterData registerData, int value)
         {
             int totalMemorySize = registerData.Offset + registerData.Size;
 
-            ulong finalValue = (ulong)(value) << registerData.Offset;
+            ulong finalValue = (ulong)value << registerData.Offset;
             if (registerMemoryList.Count == 0 ||
                 totalMemorySize > RegisterSize)
             {
                 registerMemoryList.Add(default);
-                finalValue = (ulong)(value);
+                finalValue = (ulong)value;
             }
             registerMemoryList.Span[registerMemoryList.Count - 1] += finalValue;
         }
 
-        public T GetData(RegisterData registerData) 
+        public T GetData(RegisterData registerData)
         {
             int totalMemorySize = registerData.Offset + registerData.Size;
             int registerIndex = totalMemorySize / RegisterSize;
 
             ulong currentRegister = registerMemoryList.Span[registerIndex];
-            int currentOffset = registerData.Offset - (registerIndex * RegisterSize);
+            int currentOffset = registerData.Offset - registerIndex * RegisterSize;
 
             int typeSize = (int)Math.Pow(2, registerData.Size) - 1;
-            int returnData = (int)((long)(currentRegister >> ((Math.Abs(currentOffset) + currentOffset) / 2))
-                & (typeSize) & (typeSize));
+            int returnData = (int)((long)(currentRegister >> (Math.Abs(currentOffset) + currentOffset) / 2)
+                & typeSize & typeSize);
             return (T)(dynamic)returnData; //TODO: Make sure, to avoid the dynamic cast
         }
 
-        public void Dispose() 
+        public void Dispose()
         {
             registerMemoryList.Dispose();
         }
