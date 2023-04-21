16d0caaf8c98900f6e3c69212f9409508eab41ed
Ma-tes <msestaubr@gmail.com>
2023-04-21 20:01:18 +0200

    Implemented uncomeplete parsing of call statement

--- src/Parsing/Nodes/Statements/CallStatement.cs
+++ src/Parsing/Nodes/Statements/CallStatement.cs
@@ -1,6 +1,7 @@
 ﻿using Athena.NET.Lexing;
 using Athena.NET.Lexing.Structures;
 using Athena.NET.Parsing.Interfaces;
+using Athena.NET.Parsing.Nodes.Operators;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Athena.NET.Parsing.Nodes.Statements;
@@ -18,6 +19,25 @@ internal sealed class CallStatement : StatementNode
 
     protected override bool TryParseRigthNode([NotNullWhen(true)] out NodeResult<INode> nodeResult, ReadOnlySpan<Token> tokens)
     {
+        int semicolonIndex = tokens.IndexOfToken(TokenIndentificator.Semicolon);
         return base.TryParseRigthNode(out nodeResult, tokens);
+    }
+
+    private static IEnumerable<INode> GetArgumentsNodes(ReadOnlySpan<Token> argumentsTokens)
+    {
+        int currentSeparatorIndex = argumentsTokens.IndexOfToken(TokenIndentificator.Separator);
+        while (currentSeparatorIndex != -1)
+        {
+            ReadOnlySpan<Token> argumentTokens = argumentsTokens[..currentSeparatorIndex];
+            argumentsTokens = argumentsTokens[(currentSeparatorIndex + 1)..];
+        }
+    }
+
+    private static INode GetArgumentNode(ReadOnlySpan<Token> argumentTokens)
+    {
+        int operatorIndex = OperatorHelper.IndexOfOperator(argumentTokens);
+        if (OperatorHelper.TryGetOperator(out OperatorNode operatorNode, argumentTokens[operatorIndex].TokenId))
+            return operatorNode;
+        return argumentTokens.GetDataNode();
     }
 }
