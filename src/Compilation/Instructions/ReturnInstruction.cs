c8249b83b5909a99e6a2df47238d38ba0a9d6aab
Ma-tes <msestaubr@gmail.com>
2023-06-01 21:36:44 +0200

    Created uncomplete return instruction

---
+++ src/Compilation/Instructions/ReturnInstruction.cs
@@ -0,0 +1,16 @@
+﻿using Athena.NET.Compilation.Instructions.Structures;
+using Athena.NET.Compilation.Interpreter;
+using Athena.NET.Parsing.Nodes.Statements;
+
+namespace Athena.NET.Compilation.Instructions;
+
+internal sealed class ReturnInstruction : IInstruction<ReturnStatement>
+{
+    public bool EmitInstruction(ReturnStatement node, InstructionWriter writer)
+    {
+        DefinitionData currentDefinitionData = writer.CurrentDefinitionData;
+    }
+
+    public bool InterpretInstruction(ReadOnlySpan<uint> instructions, VirtualMachine writer) =>
+        false;
+}
