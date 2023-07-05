5a555f398e49145e42f4515fe07fa0c0b8f4d2cc
Ma-tes <msestaubr@gmail.com>
2023-07-05 19:43:45 +0200

    Added incomplete ResultMemory for storing individual IResultProviders

---
+++ src/ExceptionResult/ResultMemory.cs
@@ -0,0 +1,36 @@
+﻿using Athena.NET.ExceptionResult.Interfaces;
+using System.Collections;
+
+namespace Athena.NET.ExceptionResult;
+
+//TODO: Add ICollection with a propriete methods
+//and try to reduce redudant allocations.
+internal sealed class ResultMemory<T> : IEnumerable<IResultProvider<T>>
+{
+    private readonly List<IResultProvider<T>> resultValues = new();
+    private Action<ErrorResult<T>>? onErrorResult;
+
+    public int ErrorResultIndex { get; private set; }
+    public int Count => resultValues.Count;
+
+    public ResultMemory() { }
+    public ResultMemory(Action<IResultProvider<T>> onErrorResult) 
+    {
+        this.onErrorResult = onErrorResult;
+    }
+
+    public bool AddResult(IResultProvider<T> result) 
+    {
+        resultValues.Add(result);
+        bool isErrorResult = result is ErrorResult<T>;
+
+        if (isErrorResult && onErrorResult is not null)
+            onErrorResult.Invoke((ErrorResult<T>)result);
+        return isErrorResult;
+    }
+
+    public IEnumerator<IResultProvider<T>> GetEnumerator() =>
+        resultValues.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() =>
+        ((IEnumerable)resultValues).GetEnumerator();
+}
